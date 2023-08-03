67c323153eb6bdb22ed4993610d4bdc70132fdd2
Vlada <vlada_baranovao8@mail.ru>
2023-08-03 17:27:41 +0300

    Final without images, fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
-const {src, dest, watch, parallel}  = require('gulp');
-//const src = a.src;
-//const dest = a.dest;
+const {src, dest, watch, parallel, series}  = require('gulp');
+
 
 const scss = require('gulp-sass')(require('sass'));
-
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
 const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
 
 function prepareScripts(){
     return src(['app/js/*.js', '!app/js/main.min.js'])
@@ -27,7 +26,7 @@ function prepareStyles() {
 function observe(){
     watch(['app/scss/style.scss'], prepareStyles)
     watch(['app/js/*.js', '!app/js/main.min.js'], prepareScripts)
-    watch(['app/*.html']).on('change', browserSync.reload)
+    watch(['app/**/*.html']).on('change', browserSync.reload)
 }
 
 function browserRefresh(){
@@ -37,8 +36,27 @@ function browserRefresh(){
         }
     });
 }
+
+function cleanDist(){
+    return src('dist/*')
+    .pipe(clean())
+}
+
+ 
+function build(){
+    return src([
+        'app/css/style.min.css',
+        'app/js/*.min.js',
+        'app/**/*.html'
+    ], {base: 'app'})
+    .pipe(dest('dist'))
+}
+
 exports.styles = prepareStyles;
 exports.scripts = prepareScripts;
 exports.observe = observe;
 exports.browser = browserRefresh;
-exports.default = parallel(prepareStyles, prepareScripts, browserRefresh, observe);
+exports.clean = cleanDist;
+
+exports.build = series(cleanDist, build);
+exports.default = series(prepareStyles, prepareScripts, browserRefresh, observe);
